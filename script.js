6383871f8e1c79f6a8bcd1faabf95b8a0298d37c
Caleb Ortiz <calebortiz288@gmail.com>
2025-02-07 07:55:37 -0600

    Added h1 text change

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Get the button and body elements
 const toggleButton = document.getElementById('toggleButton');
+const h1Button = document.getElementById('toggleh1');
 const body = document.body;
 
 // Add event listener to the button
@@ -10,7 +11,10 @@ toggleButton.addEventListener('click', function () {
     // Update button text based on the current mode
     if (body.classList.contains('dark-mode')) {
         toggleButton.textContent = 'Cambiar a modo claro';
+        toggleh1.textContent = 'De regreso a la luz!';
     } else {
         toggleButton.textContent = 'Cambiar a modo oscuro';
+        toggleh1.textContent = 'Hacia la oscuridad!';
+
     }
 });
